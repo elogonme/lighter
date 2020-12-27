bde8cd026338682e33b939221ab7f5173abd1563
Eldar <elogon@gmail.com>
2020-12-27 05:59:34 -0500

    Add set state feature to light class

--- Light.js
+++ Light.js
@@ -4,7 +4,8 @@ class Light {
     constructor (id = 'd073d54171f5') {
         this.id = id;
     }
-    listLights = async () => { 
+
+    async listLights() { 
         const myHeaders = new Headers();
         myHeaders.append("Authorization", `Bearer ${APIKey}`);
     
@@ -24,12 +25,11 @@ class Light {
             } catch(error) {
               console.log(error);
             }
-        }
+        };
     
-        async toggleLight() {
+    async toggleLight() {
         const myHeaders = new Headers();
         myHeaders.append("Authorization", `Bearer ${APIKey}`);
-        console.log(this.id);
         const requestOptions = {
             method: 'POST',
             headers: myHeaders,
@@ -39,12 +39,39 @@ class Light {
         try {
             const response = await fetch(`https://api.lifx.com/v1/lights/${this.id}/toggle`, requestOptions);
             if (response.ok) {
-                const jsonResponse = await response.json();
-                console.log(jsonResponse);
+                const jsonResponse = await response.json(); 
+                console.log(`Light with id: ${jsonResponse.results[0].id}, label: ${jsonResponse.results[0].label} has been turned ${jsonResponse.results[0].power}...`);
                 return jsonResponse;
               } throw new Error('Request failed!')
             } catch(error) {
               console.log(error);
-            }
-    }
-}
+        };
+    };
+
+    async setState(power = 'on', color = 'blue') {
+        const state = {
+            power: power,
+            color: color,
+            fast: false
+        }
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", `Bearer ${APIKey}`);
+        myHeaders.append("Content-type", `application/json`);
+        const requestOptions = {
+            method: 'PUT',
+            headers: myHeaders,
+            body: JSON.stringify(state),
+            redirect: 'follow'
+          };
+    
+        try {
+            const response = await fetch(`https://api.lifx.com/v1/lights/${this.id}/state`, requestOptions);
+            if (response.ok) {
+                const jsonResponse = await response.json(); 
+                return jsonResponse;
+              } throw new Error('Request failed!')
+            } catch(error) {
+              console.log(error);
+        };
+    };
+};
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+$(document).ready(function() {
+    const light = new Light('d073d54171f5');
 
-const light = new Light('d073d54171f5');
+    // light.setState('off');
+    // Light power toggle button listener
+    $('#power').on('click', () => {
+        light.toggleLight();
+    });
+    
+    // Light color control buttons
+    $('.light-btn').on('click', function() {
+        light.setState( 'on', $(this).val());
+    });
+});
 
-// light.listLights();
-light.toggleLight();
 
