8a2d3a85293c2e86bef554aa96b2c99025cd5ddc
Eldar <elogon@gmail.com>
2020-12-29 05:02:36 -0500

    Add feature to activate scenes

--- public/Light.js
+++ public/Light.js
@@ -1,80 +1,12 @@
 class Light {
-    constructor (id = 'd073d54171f5') {
-        this.id = id;
-    }
+  constructor (id = 'd073d54171f5') {
+      this.id = id;
+  }
 
-    async listLights() { 
-        const myHeaders = new Headers();
-        // myHeaders.append("Authorization", `Bearer ${APIKey}`);
-    
-        const requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-        };
-    
-        try {
-            const response = await fetch("/all", requestOptions);
-            if (response.ok) {
-                const jsonResponse = await response.json();
-                console.log(`Light ID: ${jsonResponse[0].id}, Power state: ${jsonResponse[0].power}`);
-                return jsonResponse;
-              } throw new Error('Request failed!')
-            } catch(error) {
-              console.log(error);
-            }
-        };
-    
-    async toggleLight() {
-        const myHeaders = new Headers();
-        // myHeaders.append("Authorization", `Bearer ${APIKey}`);
-        const requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            redirect: 'follow'
-          };
-    
-        try {
-            const response = await fetch(`lights/${this.id}/toggle`, requestOptions);
-            if (response.ok) {
-                const jsonResponse = await response.json(); 
-                console.log(`Light with id: ${jsonResponse.results[0].id}, label: ${jsonResponse.results[0].label} has been turned ${jsonResponse.results[0].power}...`);
-                return jsonResponse;
-              } throw new Error('Request failed!')
-            } catch(error) {
-              console.log(error);
-        };
-    };
-
-    async setState(color, brightness) {
-        const state = {};
-        if (color) state.color = color;
-        if (brightness) state.brightness = brightness;
-        state.fast = false;
-        
-        const myHeaders = new Headers();
-        // myHeaders.append("Authorization", `Bearer ${APIKey}`);
-        myHeaders.append("Content-type", `application/json`);
-        const requestOptions = {
-            method: 'PUT',
-            headers: myHeaders,
-            body: JSON.stringify(state),
-            redirect: 'follow'
-          };
-    
-        try {
-            const response = await fetch(`lights/${this.id}/state`, requestOptions);
-            if (response.ok) {
-                const jsonResponse = await response.json(); 
-                return jsonResponse;
-              } throw new Error('Request failed!')
-            } catch(error) {
-              console.log(error);
-        };
-    };
-
-    async listScenes() { 
+  async listLights() { 
       const myHeaders = new Headers();
+      // myHeaders.append("Authorization", `Bearer ${APIKey}`);
+  
       const requestOptions = {
       method: 'GET',
       headers: myHeaders,
@@ -82,14 +14,109 @@ class Light {
       };
   
       try {
-          const response = await fetch("/scenes", requestOptions);
+          const response = await fetch("/all", requestOptions);
           if (response.ok) {
               const jsonResponse = await response.json();
+              console.log(`Light ID: ${jsonResponse[0].id}, Power state: ${jsonResponse[0].power}`);
               return jsonResponse;
             } throw new Error('Request failed!')
           } catch(error) {
             console.log(error);
           }
       };
-};
+  
+  async toggleLight() {
+      const myHeaders = new Headers();
+      // myHeaders.append("Authorization", `Bearer ${APIKey}`);
+      const requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          redirect: 'follow'
+        };
+  
+      try {
+          const response = await fetch(`lights/${this.id}/toggle`, requestOptions);
+          if (response.ok) {
+              const jsonResponse = await response.json(); 
+              console.log(`Light with id: ${jsonResponse.results[0].id}, label: ${jsonResponse.results[0].label} has been turned ${jsonResponse.results[0].power}...`);
+              return jsonResponse;
+            } throw new Error('Request failed!')
+          } catch(error) {
+            console.log(error);
+      };
+  };
 
+  async setState(color, brightness) {
+      const state = {};
+      if (color) state.color = color;
+      if (brightness) state.brightness = brightness;
+      state.fast = false;
+      
+      const myHeaders = new Headers();
+      // myHeaders.append("Authorization", `Bearer ${APIKey}`);
+      myHeaders.append("Content-type", `application/json`);
+      const requestOptions = {
+          method: 'PUT',
+          headers: myHeaders,
+          body: JSON.stringify(state),
+          redirect: 'follow'
+        };
+  
+      try {
+          const response = await fetch(`lights/${this.id}/state`, requestOptions);
+          if (response.ok) {
+              const jsonResponse = await response.json(); 
+              return jsonResponse;
+            } throw new Error('Request failed!')
+          } catch(error) {
+            console.log(error);
+      };
+  };
+
+  async listScenes() { 
+    const myHeaders = new Headers();
+    const requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+    };
+
+    try {
+        const response = await fetch("/scenes", requestOptions);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            return jsonResponse;
+          } throw new Error('Request failed!')
+        } catch(error) {
+          console.log(error);
+        }
+    };
+  
+    async setScene(sceneUuid) {
+      const state = {};
+      state.scene_uuid = sceneUuid;
+      state.fast = false;
+      
+      const myHeaders = new Headers();
+      // myHeaders.append("Authorization", `Bearer ${APIKey}`);
+      myHeaders.append("Content-type", `application/json`);
+      const requestOptions = {
+          method: 'PUT',
+          headers: myHeaders,
+          body: JSON.stringify(state),
+          redirect: 'follow'
+        };
+  
+      try {
+          const response = await fetch(`scenes/scene_id:${sceneUuid}/activate`, requestOptions);
+          if (response.ok) {
+              const jsonResponse = await response.json(); 
+              return jsonResponse;
+            } throw new Error('Request failed!')
+          } catch(error) {
+            console.log(error);
+      };
+  };
+  
+};
+  
--- public/index.js
+++ public/index.js
@@ -2,15 +2,32 @@ $(document).ready(function() {
     const templateSource = $('#light-control').html();
     let template = Handlebars.compile(templateSource);
     const source = {
-        colors: ['white', 'red', 'orange', 'yellow', 'cyan', 'green', 'blue', 'purple', 'pink']
-      }
+        buttons: [{ name: 'white', uuid: 'white'}, { name: 'red', uuid: 'red'}, 
+        { name: 'orange', uuid: 'orange'}, { name: 'yellow', uuid: 'yellow'}, 
+        { name: 'cyan', uuid: 'cyan'}, { name: 'green', uuid: 'green'},
+        { name: 'blue', uuid: 'blue' }, { name: 'purple', uuid: 'purple'}, { name: 'pink', uuid: 'pink'}]
+      };
 
+    let sceneSource = {};
+      
     const compiledHtml = template(source);
     $('#light-buttons').html(compiledHtml);
     
     const light = new Light('d073d54171f5');
-    let powerState = false;
-    let state = 'off';
+    // Get predefined light scenes and output buttons
+    light.listScenes().then(scenes => {
+        return scenes.map(element => {
+            return { name: element.name.toLowerCase(), uuid: element.uuid };
+        });
+    }).then(scenesArr => {
+        sceneSource.buttons = scenesArr;
+        const html = template(sceneSource);
+        $('#scene-buttons').html(html);
+        $('#scene-buttons').children().on('click', function() {
+            light.setScene($(this).val());
+        });
+    });
+
     // Light power toggle button listener
     $('#power').on('click', () => {
         light.toggleLight();
@@ -18,13 +35,18 @@ $(document).ready(function() {
     
     // Light color control buttons
     $('.light-btn').on('click', function() {
-        light.setState( $(this).val());
+        light.setState($(this).val());
+        console.log($(this).val());
     });
 
+    // Dimmer control slider
     $('#dimmer').on('click', function(){
         const brightness = Math.round($(this).val() / 10) / 10;
         light.setState(null, brightness);
     });
+
+    // Light scenes buttons
+    
 });
 
 
